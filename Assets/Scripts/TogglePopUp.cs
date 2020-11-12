9fa15b05f625028c0fec0db11bf6f73a645c463b
rashfordcholston <carltongstale@gmail.com>
2020-11-12 15:11:19 +0100

    Adds togglescript and trainerprefab

---
+++ Assets/Scripts/TogglePopUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TogglePopUp : MonoBehaviour{
+    public GameObject popup;
+    
+    public void OpenPopup(){
+        if (popup.activeSelf == false){
+            popup.SetActive(true);
+        }
+    }
+
+    public void ClosePopup(){
+        if (popup.activeSelf){
+            popup.SetActive(false);
+        }
+    }
+}
