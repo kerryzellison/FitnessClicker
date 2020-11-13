cc02e9b8faa3e46bf7cd81965fef7ec9900fceb3
rashfordcholston <carltongstale@gmail.com>
2020-11-13 11:26:55 +0100

    added better scripts

--- Assets/Scripts/Calories.cs
+++ Assets/Scripts/Calories.cs
@@ -4,28 +4,23 @@ using UnityEngine.UI;
 public class Calories : MonoBehaviour
 {
     public Text caloriesAmount;
-    public static int calories;
-    public int counter = 0;
-
-    public void Start()
-    {
-        calories = 0;
-    }
+    public int calories;
+    public int clickCounter = 0;
 
     public void Update() 
     {
-        caloriesAmount.text = "Calories Burned: " + calories; // checks how many calories you have earned 
-
-        if (counter >= 3)
+        if (clickCounter >= 3)
         {
-            calories += 3; // if the player clicks 3 times they get 1 calorie added and resets the counter to 0
-            counter -= 3;
-        }  
-        
+            UpdateAmountText();
+            clickCounter = 0;
+        }
     }
-
+    void UpdateAmountText(){
+        calories += 3; // if the player clicks 3 times they get 3 calorie added and resets the counter to 0
+        caloriesAmount.text = $"Calories Burned: {this.calories}"; // checks how many calories you have earned 
+    }
     public void Click()
     {
-        counter++;   // adds 1 to the counter so the scripts know how much the player has pressed
+        clickCounter++;   // adds 1 to the counter so the scripts know how much the player has pressed
     }
 }
---
+++ Assets/Scripts/CaloriesBurned.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CaloriesBurned : MonoBehaviour
+{
+    public Text caloriesBurned;
+    public int caloriesMain;
+    public Calories calories;
+    public void Update()
+    {
+        caloriesMain = calories.calories;
+        caloriesBurned.text = "Todays Calories Burned: " + caloriesMain ;
+        
+    }
+}
