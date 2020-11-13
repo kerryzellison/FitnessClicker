ec534dd888717b19e474dc8318d1d16a9982ade8
rashfordcholston <carltongstale@gmail.com>
2020-11-13 15:12:38 +0100

    starting on savemanager script

---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO; // allows us to save and save on current computer
+using System.Xml; //we are using xml files to save and load 
+using System.Xml.Serialization;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager instance;
+    public SaveData activeSave;
+
+    public bool hasLoaded;
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        instance = this;
+        Load();
+    }
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Save();
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Load();
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            DeleteSaveData();
+        }
+    }
+
+    public void Save()
+    {
+        string dataPath = Application.persistentDataPath;
+
+        var serializer = new XmlSerializer(typeof(SaveData));
+        var stream = new FileStream(dataPath + "/" + activeSave.saveName + ".save", FileMode.Create);
+        serializer.Serialize(stream, activeSave);
+        stream.Close();
+
+        Debug.Log("Saved");
+    }
+
+    public void Load()
+    {
+        string dataPath = Application.persistentDataPath;
+
+        if (System.IO.File.Exists(dataPath + "/" + activeSave.saveName + ".save"))
+        {
+            var serializer = new XmlSerializer(typeof(SaveData));
+            var stream = new FileStream(dataPath + "/" + activeSave.saveName + ".save", FileMode.Open);
+            activeSave = serializer.Deserialize(stream) as SaveData;
+            stream.Close();
+
+            Debug.Log("Loaded");
+
+            hasLoaded = true;
+        }
+    }
+
+    public void DeleteSaveData()
+    {
+        string dataPath = Application.persistentDataPath;
+        if (System.IO.File.Exists(dataPath + "/" + activeSave.saveName + ".save"))
+        {
+            File.Delete(dataPath + "/" + activeSave.saveName + ".save");
+        }
+    }
+}
+
+[System.Serializable]
+public class SaveData
+{
+    public string saveName;
+
+    public Vector3 respawnPosition;
+
+    public int calories;
+}
+
