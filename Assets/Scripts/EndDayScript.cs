b3eb046c26b71d894332e5f55ab39251267ed8b2
rashfordcholston <carltongstale@gmail.com>
2020-11-13 11:56:13 +0100

    Worked Out for X days Changes
    Combined savings script with days worked out script, removed empty endday script

--- Assets/Scripts/EndDayScript.cs
+++ Assets/Scripts/EndDayScript.cs
@@ -4,10 +4,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SavingsText : MonoBehaviour
+public class EndDayScript : MonoBehaviour
 {
     public Text savingsText;
     public double savings;
+    public Text workedOutForText;
+    public int workedOutDaysAmount;
 
     public void Start()
     {
@@ -17,10 +19,12 @@ public class SavingsText : MonoBehaviour
     public void Update()
     {
         savingsText.text = "Savings: " + savings;
+        workedOutForText.text = "You've been a member for: " + workedOutDaysAmount + " Days";
     }
 
    public void Click() // when end day is pressed player will get X amount of money
     {
         savings += 1000;
+        workedOutDaysAmount++;
     }
 }
