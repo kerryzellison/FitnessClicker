e23b73251901de6a138e198db219f442ea563a8a
rashfordcholston <carltongstale@gmail.com>
2020-11-16 14:47:40 +0100

    Added Weekly Calorie Counter
    Still needs to connect to daysystem

--- Assets/Scripts/EndDayScript.cs
+++ Assets/Scripts/EndDayScript.cs
@@ -12,7 +12,7 @@ public class EndDayScript : MonoBehaviour
     public int workedOutDaysAmount;
     public Calories calories;
     public CaloriesBurned kcalsBurnedText;
-
+    public WeeklyCalories WeeklyCalories;
     public void Start()
     {
         savings = 0;
@@ -24,12 +24,16 @@ public class EndDayScript : MonoBehaviour
         workedOutForText.text = "You've been a member for: " + workedOutDaysAmount + " Days";
     }
 
-   public void Click() // when end day is pressed player will get X amount of money
+    public void Click() // when end day is pressed player will get X amount of money
     {
+        
         savings += 1000;
         workedOutDaysAmount++;
+        WeeklyCalories.SavedWeeklyCalories += calories.calories;
         calories.calories = 0;
         kcalsBurnedText.caloriesBurned.text = calories.calories.ToString();
+        WeeklyCalories.DayCounter++;
+        
     }
-   
+
 }
---
+++ Assets/Scripts/WeeklyCalories.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeeklyCalories : MonoBehaviour
+{
+    public int DayCounter;
+    public Text ThisWeekCaloriesText;
+    int WeeklyCaloriesAmount;
+    public int SavedWeeklyCalories;
+    public Calories calories;
+    void Update()
+    {
+        WeeklyCaloriesAmount = calories.calories + SavedWeeklyCalories;
+        ThisWeekCaloriesText.text = "Calories Burned This Week: " + WeeklyCaloriesAmount;
+        OnWeekEnd();
+        
+
+    }
+
+    public void OnWeekEnd()
+    {
+        if (DayCounter > 7)
+        {
+            DayCounter = 1;
+            SavedWeeklyCalories = 0;
+
+        }
+    }
+    
+    
+}
