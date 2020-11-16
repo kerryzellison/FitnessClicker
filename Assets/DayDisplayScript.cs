3c353a2200f0674b76232c68fdb80abc505b280d
rashfordcholston <carltongstale@gmail.com>
2020-11-16 16:57:06 +0100

    Adds days function
    Adds daytimer and end day button function with counting amount of days passed.

---
+++ Assets/DayDisplayScript.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Clicker.Player;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayDisplayScript : MonoBehaviour{
+    public DaySystemScript timerScript;
+    public long startTime;
+    public Text timeText;
+    public Text numberOfDays;
+    public long milliseconds;
+    public int seconds;
+    public int days = 1;
+    private TimeSpan timeSpan;
+
+    public void Start(){
+        startTime = timerScript.GetEpochTimeMilliseconds();
+        numberOfDays.text = $"You have been a member for: {days} days";
+    }
+    public void Update(){
+        milliseconds = timerScript.GetEpochTimeMilliseconds() - startTime;
+        seconds = (int) TimeSpan.FromMilliseconds(milliseconds).TotalSeconds;
+        timeSpan = TimeSpan.FromSeconds(seconds * 48); // times 48 for 30 min real time
+        if (timeSpan.Days == 1){
+            resetTimer();
+        }
+        timeText.text = $"Time - {timeSpan}";
+    }
+    void resetTimer(){
+        startTime = timerScript.GetEpochTimeMilliseconds();
+        days++;
+        numberOfDays.text = $"You have been a member for: {days} days";
+    }
+
+    public void ResetDay(){
+        resetTimer();
+    }
+}
--- Assets/Scripts/DaySystemScript.cs
+++ Assets/Scripts/DaySystemScript.cs
@@ -4,29 +4,23 @@ using UnityEngine;
 using System;
 using UnityEngine.UI;
 
-public class DaySystemScript : MonoBehaviour
-{
-    public class DateTimeTest : MonoBehaviour{
-        long GetEpochTimeMilliseconds(){
-            DateTime startOfTime = new DateTime(1970, 1, 1, 0, 0, 0);
-            var time = Convert.ToInt64(DateTime.UtcNow.Subtract(startOfTime).TotalMilliseconds);
-            return time;
-        }
-
-        public long lastCalorieBurnTime;
-        public void Start(){
-            lastCalorieBurnTime = GetEpochTimeMilliseconds();
-        }
-
-        void Update(){
-            var millisecondsPassed = GetEpochTimeMilliseconds() - lastCalorieBurnTime;
-            var secondsPassed = millisecondsPassed / 1000;
-            if (secondsPassed >= 60 * 30){ // 60 sec * 30 = 30 minutes.
-                lastCalorieBurnTime = GetEpochTimeMilliseconds(); // reset the timer.
-            }
-            Debug.Log(secondsPassed.ToString());
-        }
+public class DaySystemScript : MonoBehaviour{
+    public long GetEpochTimeMilliseconds(){
+        DateTime startOfTime = new DateTime(1970, 1, 1, 0, 0, 0);
+        var time = Convert.ToInt64(DateTime.UtcNow.Subtract(startOfTime).TotalMilliseconds);
+        return time;
     }
 
-   
+    public long lastCalorieBurnTime;
+    public void Start(){
+        lastCalorieBurnTime = GetEpochTimeMilliseconds();
+    }
+
+    void Update(){
+        var millisecondsPassed = GetEpochTimeMilliseconds() - lastCalorieBurnTime;
+        var secondsPassed = millisecondsPassed / 1000;
+        if (secondsPassed >= 60 * 30){ // 60 sec * 30 = 30 minutes.
+            lastCalorieBurnTime = GetEpochTimeMilliseconds(); // reset the timer.
+        }
+    }
 }
