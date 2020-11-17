a372ec24ddf78880828e71549d6fe422b1f18b37
rashfordcholston <carltongstale@gmail.com>
2020-11-17 15:54:32 +0100

    Added BuyButton for trainers

---
+++ Assets/Scripts/TrainersBuying.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrainersBuying : MonoBehaviour
+{
+   GameObject HolderForTrainerCards;
+   GameObject g;
+   [SerializeField] private Transform ShopScrollView;
+
+   void Start()
+   {
+       HolderForTrainerCards = ShopScrollView.GetChild(0).gameObject;
+
+       for (int i = 0; i < 8; i++)
+       {
+           g = Instantiate(HolderForTrainerCards, ShopScrollView);
+       }
+       Destroy(HolderForTrainerCards);
+       
+   }    
+   }
