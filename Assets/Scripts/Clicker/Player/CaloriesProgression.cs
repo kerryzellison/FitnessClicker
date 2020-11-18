f37acc43c530be576fc5ffffee63bf5a3c64c504
rashfordcholston <carltongstale@gmail.com>
2020-11-18 16:44:35 +0100

    added animations to gym scene

--- Assets/Scripts/Clicker/Player/CaloriesProgression.cs
+++ Assets/Scripts/Clicker/Player/CaloriesProgression.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Clicker.Player{
     public class CaloriesProgression : MonoBehaviour{
         public PlayerData playerData;
         public int clickCounter = 0;
         public ProgressBar progressBar;
+        public Sprite[] CharacterSprites;
+        public Sprite[] characterSprite1;
+        public Sprite[] characterSprite2;
+        public Sprite[] characterSprite3;
+        public Sprite[] characterSprite4;
+        public Image renderer;
 
         private void Start(){
             progressBar.SetProgress(0);
             progressBar.SetMaxProgress(3);
+            renderer.sprite = CharacterSprites[clickCounter];
         }
 
         private void Update(){
@@ -26,6 +34,7 @@ namespace Clicker.Player{
         {
             clickCounter++;
             progressBar.SetProgress(clickCounter);
+            renderer.sprite = CharacterSprites[clickCounter];
         }
     }
 }
